77e2fc131843845681e7f4a722b0bd0c2128ce56
Keval <keval.langalia@live.com>
2021-02-11 04:21:21 +0530

    updated SearchResult with the necessary properties

--- app/src/main/java/com/example/booksearch/network/SearchResult.kt
+++ app/src/main/java/com/example/booksearch/network/SearchResult.kt
@@ -1,7 +1,81 @@
 package com.example.booksearch.network
 
+import android.os.Parcelable
+import android.view.textclassifier.TextLinks
+import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class SearchResult {
+data class SearchResult(
+
+    val totalItems: Int,
+    val items: List<Book>
+
+) : Parcelable {
+
+}
+
+@Parcelize
+data class Book(
+    val id: String,
+    val selfLink: String,
+
+    @Json(name = "volumeInfo")
+    val bookInfo: BookInfo,
+
+    val saleInfo: BookSaleInfo
+
+) : Parcelable {
+
+}
+
+@Parcelize
+data class BookInfo(
+    val title: String,
+    val subTitle: String,
+    val authors: List<String>,
+    val publisher: String,
+    val publishedDate: String,
+    val description: String,
+    val pageCount: Int,
+    val averageRating: Double,
+    val ratingsCount: Int,
+
+    @Json(name = "imageLinks")
+    val thumbnailLinks: ThumbnailLinks,
+
+    val language: String,
+    val infoLink: String
+) : Parcelable {
+
+}
+
+@Parcelize
+data class ThumbnailLinks(
+    val thumbnail: String,
+    val smallThumbnail: String
+) : Parcelable {
+
+}
+
+@Parcelize
+data class BookSaleInfo(
+    val country: String,
+
+    @Json(name = "listPrice")
+    val actualPrice: BookPrice,
+
+    @Json(name = "retailPrice")
+    val sellingPrice: BookPrice
+
+) : Parcelable {
+
+}
+
+@Parcelize
+data class BookPrice(
+    val amount: Double,
+    val currencyCode: String
+) : Parcelable {
+
 }

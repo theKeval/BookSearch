f1ba179bf3b1b46d5c3a0777baa1c4df3ba4cdcf
Keval <keval.langalia@live.com>
2021-02-11 03:55:26 +0530

    again dependencies added & started adding network classes

---
+++ app/src/main/java/com/example/booksearch/network/SearchResult.kt
@@ -0,0 +1,7 @@
+package com.example.booksearch.network
+
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class SearchResult {
+}
---
+++ app/src/main/java/com/example/booksearch/viewmodels/SearchViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.booksearch.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel: ViewModel() {
+
+
+
+}

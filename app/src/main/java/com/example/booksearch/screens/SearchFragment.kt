279ca23b6c40dccaf01c27e97fcf677c32b4a6e7
Keval <keval.langalia@live.com>
2021-02-11 03:12:13 +0530

    slight modification

--- app/src/main/java/com/example/booksearch/screens/SearchFragment.kt
+++ app/src/main/java/com/example/booksearch/screens/SearchFragment.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.booksearch.R
+import com.example.booksearch.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        //any of below line will work, but I'm not sure what is the difference :(
+        // binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container,false)
+        binding = FragmentSearchBinding.inflate(inflater)
+
+        return binding.root
     }
 }

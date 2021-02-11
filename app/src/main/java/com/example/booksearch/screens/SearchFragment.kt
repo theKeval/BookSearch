ebf8aa7ab21d79e0dce84851cb4d607291f26f64
Keval <keval.langalia@live.com>
2021-02-11 05:51:03 +0530

    major
    - search viewmodel code added
    - binding connections

--- app/src/main/java/com/example/booksearch/screens/SearchFragment.kt
+++ app/src/main/java/com/example/booksearch/screens/SearchFragment.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.booksearch.R
 import com.example.booksearch.databinding.FragmentSearchBinding
+import com.example.booksearch.viewmodels.SearchViewModel
 
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+
+    private val viewModel: SearchViewModel by lazy {
+        ViewModelProvider(this).get(SearchViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,9 +25,18 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        //any of below line will work, but I'm not sure what is the difference :(
+        // any of below line will work, but I'm not sure what is the difference :(
+        // now i know the difference :)
+        // https://stackoverflow.com/questions/51893495/android-databinding-databindingutil-vs-binding-class
+        // use generated databinding class if available - recommended by documentation
+
         // binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container,false)
         binding = FragmentSearchBinding.inflate(inflater)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+
 
         return binding.root
     }
--- app/src/main/java/com/example/booksearch/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/example/booksearch/viewmodels/SearchViewModel.kt
@@ -1,9 +1,57 @@
 package com.example.booksearch.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.booksearch.network.BooksApi
+import com.example.booksearch.network.SearchResult
+import kotlinx.coroutines.launch
+
+enum class BooksApiStatus { LOADING, ERROR, DONE }
 
 class SearchViewModel: ViewModel() {
 
+    private var _status = MutableLiveData<BooksApiStatus>()
+    val status: LiveData<BooksApiStatus>
+        get() = _status
+
+    private var _searchText = MutableLiveData<String>()
+    val searchText: LiveData<String>
+        get() =_searchText
+
+    private var _searchResult = MutableLiveData<SearchResult>()
+    val searchResult: LiveData<SearchResult>
+        get() = _searchResult
+
+    init {
+        searchBooks(searchText.value)
+    }
+
+    private fun searchBooks(text: String?) {
+        if (!text.isNullOrEmpty()) {
+
+            viewModelScope.launch {
+                _status.value = BooksApiStatus.LOADING
+
+                try {
+                    _searchResult.value = BooksApi.retrofitService.getBooks(text)
+                    _status.value = BooksApiStatus.DONE
+                }
+                catch (t: Throwable) {
+                    _status.value = BooksApiStatus.ERROR
+                    _searchResult.value = SearchResult(0, ArrayList())
+                }
+
+            }
+
+        }
+    }
+
+
+    fun newSearch(text: String) {
+        searchBooks(text)
+    }
 
 
 }
